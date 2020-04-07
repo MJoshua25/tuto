ed10f2f4b6894ad47d90cecb21bc575d32a1bf07
MJoshua25 <jyao2000@yahoo.fr>
2020-04-07 19:31:18 +0000

    Avancé dans le Tuto

--- photomedia/blog/models.py
+++ photomedia/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Categorie(models.Model):
+    titre = models.CharField(max_length=50)
+    cover = models.ImageField(upload_to='categories')
+
+    class Meta:
+        verbose_name = "Categorie"
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return str(self.titre)
+
+
+class Tag(models.Model):
+    titre = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def __str__(self) -> str:
+        return str(self.titre)
+
+
+# TODO: article Paul
+
+# TODO: commentaire Daouda
--- photomedia/blog/views.py
+++ photomedia/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    data = {
+
+    }
+    return render(request, 'pages/index.html', data)
+
+
+# TODO: about, category Paul
+
+# TODO: contact, single-blog Daouda
